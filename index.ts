3b0d97fb4266db890b6885a0385f5f4a0f37ccee
TrackZone1 <48413426+TrackZone1@users.noreply.github.com>
2025-02-17 15:50:31 +0100

    fix

--- index.ts
+++ index.ts
@@ -5,8 +5,18 @@ const proxyConfig: { [key: string]: string } = {
 Bun.serve({
     port: process.env.PORT || 80,
     async fetch(req) {
+        if (req.method === "OPTIONS") {
+            return new Response(null, {
+                headers: {
+                    "Access-Control-Allow-Origin": "*",
+                    "Access-Control-Allow-Methods": "*",
+                    "Access-Control-Allow-Headers": "*",
+                },
+            });
+        }
+
         const url = new URL(req.url);
-        const host = url.hostname;
+        const host = url.host;
 
         const targetUrl = proxyConfig[host];
         if (!targetUrl) {
@@ -17,12 +27,16 @@ Bun.serve({
 
         const newRequest = new Request(targetURL, {
             ...req,
+            method: req.method,
+            headers: req.headers,
+            body: await req.blob(),
             redirect: "manual",
         });
 
         let res = await fetch(newRequest);
         res.headers.set("server", "BunReverseProxy");
         res.headers.delete("content-encoding");
+        res.headers.set("Access-Control-Allow-Origin", "*");
 
         return res;
     },
